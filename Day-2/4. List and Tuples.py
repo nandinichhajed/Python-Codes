0fd122269b18bf285be2e70b3a75e39d914df5df
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-08 22:00:22 +0530

    List and Tuples

---
+++ Day-2/4. List and Tuples.py
@@ -0,0 +1,16 @@
+# Write a program which accepts a sequence of comma-separated numbers 
+# from console and generate a list and a tuple which contains every number.
+# Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+# code
+n = input('Enter a series of numbers separated by a comma:')
+l = n.split(',')
+t = tuple(l)
+
+print(l)
+print(t)
