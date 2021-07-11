146379e9383105a490ce1690d6f92c9299bae646
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-11 23:27:21 +0530

    Practice Code of Day-3

---
+++ Day-3/13. Count Letters and Digits.py
@@ -0,0 +1,18 @@
+# Write a program that accepts a sentence and calculate the number of letters and digits.
+
+# Suppose the following input is supplied to the program:
+# hello world! 123
+
+# Then, the output should be:
+# LETTERS 10
+# DIGITS 3
+
+word = input()
+letter, digit = 0,0
+
+for i in word:
+    if i.isalpha(): # returns True if alphabet
+        letter += 1
+    elif i.isnumeric(): # returns True if numeric
+        digit += 1
+print(f"LETTERS {letter}\nDIGIT {digit}") # two different types of formating method is shown in both solution
