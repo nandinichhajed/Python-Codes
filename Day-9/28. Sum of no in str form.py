a5e8f45e56398a87644c3e7a291a7b4cecbf7a4f
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-16 01:49:07 +0530

    sum of no in string form

---
+++ Day-9/28. Sum of no in str form.py
@@ -0,0 +1,13 @@
+# Define a function that can receive two integer numbers in string form
+# and compute their sum and then print it in console.
+
+# code
+def sum(a, b):
+    return int(a) + int(b)
+
+
+print(sum("1", "2"))
+
+# Alternative
+# sum = lambda a,b : int(a) + int(b)
+# print(sum("1", "2"))
