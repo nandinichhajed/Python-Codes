c8b3ef00c4cd020ce459a2383c0527e6ea8d5bff
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-16 01:49:23 +0530

    concate string

---
+++ Day-9/29.Concate String.py
@@ -0,0 +1,12 @@
+# Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+# Code
+def sum(a, b):
+    return a + b
+
+
+print(sum("1", "2"))
+
+# Alternative
+# sum = lambda a,b : a + b
+# print(sum("1", "2"))
