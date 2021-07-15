85d7aa0e25fb14d6022244ecc6bf229247d0fe7f
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-16 01:48:29 +0530

    sum of no's

---
+++ Day-9/26. sum of no's.py
@@ -0,0 +1,10 @@
+# Define a function which can compute the sum of two numbers.
+def sum(a, b):
+    return a + b
+
+
+print(sum(1, 2))
+
+# Alternative
+# sum = lambda a,b : a + b
+# print (sum(1,2))
