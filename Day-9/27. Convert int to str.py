a937f9a7113d40902328ac8e43487def982fbe84
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-16 01:48:45 +0530

    convert into string

---
+++ Day-9/27. Convert int to str.py
@@ -0,0 +1,15 @@
+# Define a function that can convert a integer into a string and print it in console.
+# Code
+def convert(x):
+    return str(x)
+
+
+n = convert(10)
+print(n)
+print(type(n))
+
+# Alternative
+# convert = lambda x : str(x)
+# n = convert(10)
+# print(n)
+# print(type(n))
