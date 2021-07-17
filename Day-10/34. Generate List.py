068c62ba769973e0a173bc0be80061d51c9401bd
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-17 16:29:10 +0530

    Generate List

---
+++ Day-10/34. Generate List.py
@@ -0,0 +1,27 @@
+# Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+# Then the function needs to print the first 5 elements in the list.
+# Hint:
+# Use ** operator to get power of a number.
+# Use range() for loops.Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+
+# code
+def square():
+    li = list()
+    for i in range(1, 21):
+        li.append(i**2)
+    print(li[:5])
+
+
+square()
+
+# Alternative
+
+
+def square():
+    li = [i ** 2 for i in range(1, 21)]
+    for i in range(5):
+        print(li[i])
+
+
+square()
