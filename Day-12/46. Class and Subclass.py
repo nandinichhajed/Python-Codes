bc19d4c2cb9112da7536d7a0e20d0f9844a53fed
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-19 15:04:23 +0530

    Classes & Subclass

---
+++ Day-12/46. Class and Subclass.py
@@ -0,0 +1,19 @@
+# Define a class named American and its subclass NewYorker.
+# Hints:
+# Use class Subclass(ParentClass) to define a subclass.*
+
+# Code
+
+class American():
+    pass
+
+
+class NewYorker(American):
+    pass
+
+
+american = American()
+newyorker = NewYorker()
+
+print(american)
+print(newyorker)
