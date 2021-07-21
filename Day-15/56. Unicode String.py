6fc51325a7041ee22c3232d1c2adfbf90855aed2
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-22 00:23:13 +0530

    Unicode String

---
+++ Day-15/56. Unicode String.py
@@ -0,0 +1,8 @@
+# Print a unicode string "hello world".
+
+# Hints
+# Use u'strings' format to define unicode string.
+
+# A Unicode String is a Sequence of zero or more code points.
+UC = u"Hellow World!"
+print(UC)
