fca90960586891fbe2b91ce186f977e655e41301
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-24 14:18:59 +0530

    Generate a random Value

---
+++ Day-17/68.Generaate a random value.py
@@ -0,0 +1,13 @@
+# Please generate a random float where the value is between 10 and 100 using Python module.
+
+# Hints
+# Use random.random() to generate a random float in [0,1].
+
+# Code
+
+# import random
+# print(random.random()*100)
+
+# Alternative
+import random
+print(random.uniform(10, 100))
