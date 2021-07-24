6e0ad39783c4ac0353e91d9ed329098dcaeb705b
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-24 14:19:16 +0530

    Generate a random value

---
+++ Day-17/69.Generaate a random value.py
@@ -0,0 +1,12 @@
+# Please generate a random float where the value is between 5 and 95 using Python module.
+
+# Hints
+# Use random.random() to generate a random float in [0,1].
+
+# Code
+
+# import random
+# print(random.random()*100-5)
+
+import random
+print(random.uniform(5, 95))
