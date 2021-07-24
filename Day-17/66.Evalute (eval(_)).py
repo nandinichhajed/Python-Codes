f67d024b83e2a07faabcd25cb8203699d4f58fc1
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-24 14:17:23 +0530

    Evalute (eval(_))

---
+++ Day-17/66.Evalute (eval(_)).py
@@ -0,0 +1,15 @@
+# Please write a program which accepts basic mathematic expression
+# from console and print the evaluation result.
+
+# Example: If the following n is given as input to the program:
+# 35 + 3
+
+# Then, the output of the program should be:
+# 38
+
+# Hints
+# Use eval() to evaluate an expression.
+
+# Code
+s = input("Enter an expression to evalute: ")
+print(eval(s))
