566d02a355c3dc579eae0573bfd71878f653bf18
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-25 14:37:42 +0530

    find 5 random no's in the given range

---
+++ Day-18/72. Five random no's in range.py
@@ -0,0 +1,8 @@
+# Please write a program to generate a list with 5 random numbers between 100 and 200 inclusive.
+
+# Hints
+# Use random.sample() to generate a list of random values.
+
+# Code
+import random
+print(random.sample(range(100, 201), 5))
