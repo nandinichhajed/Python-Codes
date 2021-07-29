eb9f2754406749070db7f703119fb702f8c8211e
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-29 10:52:10 +0530

    Permutation via itertools

---
+++ Day-22/93.Permutation via itertools.py
@@ -0,0 +1,8 @@
+# Please write a program which prints all permutations of [1,2,3]
+
+# Hints
+# Use itertools.permutations() to get permutations of list.
+
+# Code
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
